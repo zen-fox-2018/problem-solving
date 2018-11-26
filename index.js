e20f45cb8913d20ed1b1760a4e2cc63e41edeaa9
ahmad <ahmad.syukron6991@gmail.com>
2018-11-26 15:45:47 +0700

    tugas problem solving gym belum kelar kurang koma

---
+++ index.js
@@ -0,0 +1,43 @@
+// pseudocode
+
+// function gymMember
+
+// var = array with value string 'budi' 'tono' and 'anton'
+
+
+function gymMember(startDate,endDate){
+    var result = ''
+    var day = endDate-startDate
+
+    var name = ['tono','anton','budi']
+    
+    var tanggal = startDate
+        for(i = 0; i <= day; i ++){
+            var cekTono = false
+            let cekAnton= false
+            let cekBudi = false
+            var holder = 'tanggal '
+                if((i + tanggal) % 5 === 0){
+                    holder +=  (i+tanggal) + ': ' +'tempat fitness tutup'
+                }
+                else {
+                    holder += (i+startDate) +': '
+                }
+                if(i % 2 === 0 && (i + tanggal) % 5 !== 0 ){
+                 holder += name[0] +', '
+                }
+                if (i % 4 === 0 && (i + tanggal) % 5 !== 0) {
+                   holder += name[1]+', '
+                }
+                if (i % 5 === 0 && (i + tanggal) % 5 !== 0) {
+                    holder += name[2] + ', '           
+                }
+
+        
+            result += holder
+            result += '\n'
+    }
+        
+    return result
+}
+    console.log(gymMember(7,31))
