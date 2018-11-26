086120de5240a217c18750c5d237140074ad5481
ACER <fauziaazia171097@gmail.com>
2018-11-26 03:56:26 -0500

    fix identation

--- index.js
+++ index.js
@@ -26,24 +26,24 @@
 
 function generateCalender(){
 
-    for(let i = 7 ; i <= 31; i++){
+    for (let i = 7 ; i <= 31; i++) {
         let print = `Tanggal ${i}:`
         let arr = []
         let awal = 7
-        if(i % 5 === 0){
-            arr.push('Tempat Fitnes Tutup')
-        }else if(i === 7){
+        if (i % 5 === 0) {
+          arr.push('Tempat Fitnes Tutup')
+        } else if (i === 7) {
             arr.push('Tono')
             arr.push('Anton')
             arr.push('Budi')
-        }else{
-            if(i % 2 !== 0 ){
+        } else {
+            if (i % 2 !== 0 ) {
                 arr.push(`Tono`)
             }
-            if((i-awal)% 4 === 0){
+            if ((i-awal)% 4 === 0) {
                 arr.push(`Anton`)
             }
-            if((i-awal) % 5 === 0){
+            if ((i-awal) % 5 === 0) {
                 arr.push(`Budi`)
             }
         } 
